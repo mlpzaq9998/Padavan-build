5a12feaf65ecab9d97e54c194635da3a419119f5
backup <591609998@qq.com>
2020-06-10 00:36:43 +0800

    Delete diy-part1.sh

--- diy-part1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -f $GITHUB_WORKSPACE/MSG1500-NAND.config /opt/rt-n56u/trunk/configs/templates/MSG1500-NAND.config
-cp -f $GITHUB_WORKSPACE/MSG1500-SPI.config /opt/rt-n56u/trunk/configs/templates/MSG1500-SPI.config
-cp -r -f $GITHUB_WORKSPACE/MSG1500-NAND /opt/rt-n56u/trunk/configs/boards/MSG1500-NAND
-cp -r -f $GITHUB_WORKSPACE/MSG1500-SPI /opt/rt-n56u/trunk/configs/boards/MSG1500-SPI
