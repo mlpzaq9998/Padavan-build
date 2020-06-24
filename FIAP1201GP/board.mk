3144b312f44319d02ea8e640b81ad78949c97c1d
backup <591609998@qq.com>
2020-06-24 10:58:25 +0800

    ADD FIAP1201GP

---
+++ FIAP1201GP/board.mk
@@ -0,0 +1,15 @@
+##################################################################
+# Board PID # Board Name       # PRODUCT # Note
+##################################################################
+# FIAP1201GP# FAST FIAP1201GP  # MT7620  #
+##################################################################
+
+CFLAGS += -DBOARD_FIAP1201GP -DVENDOR_FAST
+BOARD_NUM_USB_PORTS=0
+
+### FAST firmware description ###
+FAST_HWID=0x04DA857C
+FAST_HWREV=0x0C000600
+FAST_HWREVADD=0x04000000
+FAST_FLASHLAYOUT=8Mmtk
+FAST_HVERSION=3
